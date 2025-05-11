1788f284dd610a43af9e57320ed0151c88da7d46
Sobrielle <sgscalalay@gmail.com>
2025-05-11 22:23:26 +0800

    change the index location

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP TO-DO LIST</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./page/assets/css/style.css">
 </head>
 <body>
-        <div id="header"><img src="./assets/img/HEADER.png"></div> 
-        <a href="toodoo.php" class="button">CLICK ME TO BE PRODUCTIVE!</a>
+        <div id="header"><img src="./page/assets/img/HEADER.png"></div> 
+        <a href="./page/toodoo.php" class="button">CLICK ME TO BE PRODUCTIVE!</a>
     <!-- <?php
         
     ?> -->
